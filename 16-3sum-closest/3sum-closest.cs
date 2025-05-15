6cb2d8b7186234da40dabcef1d440199891e8630
bHavikao4 <bhavikasairanik@gmail.com>
2025-05-15 12:47:23 +0530

    Time: 18 ms (27.30%) | Memory: 43.2 MB (59.71%) - LeetSync

--- 16-3sum-closest/3sum-closest.cs
+++ 16-3sum-closest/3sum-closest.cs
@@ -3,23 +3,21 @@ using System;
 public class Solution {
     public int ThreeSumClosest(int[] nums, int target) {
         Array.Sort(nums);
-        int closestSum = nums[0] + nums[1] + nums[2]; // Initialize with first 3 elements
+        int cl= nums[0] + nums[1] + nums[2];
 
         for (int i = 0; i < nums.Length - 2; i++) {
             int l = i + 1;
             int r = nums.Length - 1;
 
             while (l < r) {
-                int currentSum = nums[i] + nums[l] + nums[r];
-
-                // Check if this sum is closer to target
-                if (Math.Abs(target - currentSum) < Math.Abs(target - closestSum)) {
-                    closestSum = currentSum;
+                int CS = nums[i] + nums[l] + nums[r];
+                if (Math.Abs(target - CS) < Math.Abs(target - cl)) {
+                    cl = CS;
                 }
 
-                if (currentSum == target) {
-                    return currentSum; // exact match
-                } else if (currentSum < target) {
+                if (CS== target) {
+                    return CS; 
+                } else if (CS < target) {
                     l++;
                 } else {
                     r--;
@@ -27,6 +25,6 @@ public class Solution {
             }
         }
 
-        return closestSum;
+        return cl;
     }
 }
